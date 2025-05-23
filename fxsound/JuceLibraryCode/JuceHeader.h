f57e64bc6fcd70723a0b7f4a550a47f937b33bc1
Vijay <18183654+bvijay74@users.noreply.github.com>
2025-05-23 16:18:22 +0530

    JUCE 8 upgrade for x86/x64

--- fxsound/JuceLibraryCode/JuceHeader.h
+++ fxsound/JuceLibraryCode/JuceHeader.h
@@ -45,7 +45,7 @@ namespace ProjectInfo
 {
     const char* const  projectName    = "FxSound";
     const char* const  companyName    = "FxSound LLC";
-    const char* const  versionString  = "1.1.32.0";
-    const int          versionNumber  = 0x1012000;
+    const char* const  versionString  = "1.1.34.0";
+    const int          versionNumber  = 0x1012200;
 }
 #endif
